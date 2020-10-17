4c4ac43d63647b8f2f8b01b94e12cfc86770bcdd
HaoWeiHe <altar396@gmail.com>
2020-10-17 11:44:18 +0800

    updated

--- demo.py
+++ demo.py
@@ -1,65 +1,27 @@
 import os
 import sys
-from nltk.tokenize import StanfordTokenizer
-from nltk.tag import StanfordNERTagger
-from nltk.tag import StanfordPOSTagger
-from nltk.parse.stanford import StanfordParser
-from nltk import Tree
-from nltk.parse.stanford import StanfordDependencyParser
 
-
-#Segmentation  
-def tokenizer(stn):
-	tokenizer = StanfordTokenizer()
-	return tokenizer.tokenize(stn)
-
-#POS parser
-def pos(stn):
-	eng_tagger = StanfordPOSTagger('english-bidirectional-distsim.tagger')
-	return eng_tagger.tag(stn)
-
-
-#NER 
-def NER(stn):
-	eng_tagger = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
-	return(eng_tagger.tag(stn))
-
-#syntactic anlysis
-def parser(stn):
-	eng_parser = StanfordParser()
-	res = list(eng_parser.parse(stn))
-	
-	# draw syntactic tree
-	res[0].draw()
-	return(res)
-
-#Depependency tree
-def dependency(stn):
-	eng_parser = StanfordDependencyParser()
-	res = list(eng_parser.parse(stn))
-	for row in res[0].triples():
-	    print(row)
-
+from practnlptools.tools import Annotator
 
 if __name__ == "__main__":
-	
-	os.environ["CLASSPATH"] = os.getcwd()+"/StanfordNLP/jars"
-	os.environ["STANFORD_MODELS"] = os.getcwd()+"/StanfordNLP/models"
-	# stn = "That U.S.A. poster-print costs $12.40..."
+
 	stn = sys.argv[2]
+	annotator = Annotator()
 
-	if sys.argv[1] == "tokenizer":
-		print(tokenizer(stn))
+	if sys.argv[1] == "pos":
+		print(annotator.getAnnotations(stn)['pos'])
+		
 
-	# if sys.argv[1] == "pos":
-	# 	print(pos(tokenizer(stn)))
-
-
-	# if sys.argv[1] == "ner":
-	# 	print(NER(tokenizer(stn)))
+	if sys.argv[1] == "srl":
+		
+		print(annotator.getAnnotations(stn)['srl'])
+	
+	if sys.argv[1] == "ner":
+		print(annotator.getAnnotations(stn)['ner'])
 
 	if sys.argv[1] == "parser":
-		print(parser(tokenizer(stn)))
+		print(annotator.getAnnotations(stn))
 
 	if sys.argv[1] == "dependency":
-		dependency(tokenizer(stn))
+
+		print(annotator.getAnnotations(stn,dep_parse=True))
